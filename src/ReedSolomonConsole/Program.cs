e4fa6a02ed57577d24fe4aa574515b92823b2a72
lol <papp12345r@gmail.com>
2026-03-23 03:49:37 +0200

    Add the Console Skeleton

---
+++ src/ReedSolomonConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//using ReedSolomon;
+
+namespace ReedSolomonConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test");
+            Console.ReadKey();
+        }
+    }
+}
